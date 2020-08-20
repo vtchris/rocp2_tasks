625b7f5d563c19457f2ed19b38e17aef1c62461c
VTC <vtchris.2357@gmail.com>
2020-08-20 09:44:26 -0400

    Added post and delete to service

--- client/src/app/services/task.service.ts
+++ client/src/app/services/task.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Todo } from '../models/Todo';
@@ -10,9 +10,26 @@ import { Todo } from '../models/Todo';
 export class TaskService {
   url:string = 'http://3.15.237.3:8080/todos';
 
+  httpHeader = {
+    headers: new HttpHeaders({
+      'Content-Type':'application/json'
+    })
+  }
+
   constructor(private http: HttpClient) { }
+
+  addTodo(todo: Todo):Observable<any>{
+    return this.http.post<Todo>(this.url,todo,this.httpHeader);
+  }
+
+  deleteTodo(todo: Todo):Observable<any>{
+    return this.http.delete(`${this.url}/${todo.id}`,this.httpHeader);
+  }
 
   getTodos(): Observable<Todo[]>{
     return this.http.get<Todo[]>(this.url);
   }
+
+  
+
 }
