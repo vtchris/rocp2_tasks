249d2c07916a2d4dcb8d1d4144a8e1b8ebe71066
Sandy Moose <sandra.moose@gmail.com>
2020-08-21 13:07:36 -0700

    Initial Routing and Landing Page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/todos', pathMatch: 'full'},
+  { path: 'todos', component: TaskListComponent},
+];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
