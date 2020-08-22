7fd3134bb8c0678ba6b4f0b39352136b60d72434
Sandy Moose <sandra.moose@gmail.com>
2020-08-22 12:20:04 -0700

    task-detail added to routing, dashboard set to link tasks to their details

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TaskListComponent } from './task-list/task-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent},
   { path: 'todos', component: TaskListComponent},
+  { path: 'todos/:id', component: TaskDetailComponent},
 ];
 
 @NgModule({
