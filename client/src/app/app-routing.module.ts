bb7ee6254b1ae3cd7dd4e41775425ae414b8ec58
Sandy Moose <sandra.moose@gmail.com>
2020-08-22 21:37:54 -0700

    Added Kanban board to router and navbar

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { TaskListComponent } from './task-list/task-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { KanbanBoardComponent } from './kanban/kanban-board/kanban-board.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent},
   { path: 'todos', component: TaskListComponent},
   { path: 'todos/:id', component: TaskDetailComponent},
+  { path: 'kanban', component: KanbanBoardComponent},
 ];
 
 @NgModule({
