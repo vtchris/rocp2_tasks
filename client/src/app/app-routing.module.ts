b876452811e9821151cb03a97423d6a52734384f
Sandy Moose <sandra.moose@gmail.com>
2020-08-24 17:59:03 -0700

    Added New Kanban Task Component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TaskListComponent } from './task-list/task-list.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
+
 import { KanbanBoardComponent } from './kanban/kanban-board/kanban-board.component';
 import { KanbanDetailComponent } from './kanban/kanban-detail/kanban-detail.component';
+import { KanbanNewTaskComponent } from './kanban/kanban-new-task/kanban-new-task.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
@@ -14,7 +18,7 @@ const routes: Routes = [
   { path: 'todos/add', component: TaskDetailComponent},
   { path: 'kanban', component: KanbanBoardComponent},
   { path: 'kanban/:id', component: KanbanDetailComponent},
-  { path: 'kanban/add', component: KanbanDetailComponent},
+  { path: 'kanban/add', component: KanbanNewTaskComponent},
 ];
 
 @NgModule({
---
+++ client/src/app/kanban/kanban-new-task/kanban-new-task.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Todo } from 'src/app/models/Todo';
+import { TaskService } from '../../services/task.service';
+
+@Component({
+  selector: 'app-kanban-new-task',
+  templateUrl: './kanban-new-task.component.html',
+  styleUrls: ['./kanban-new-task.component.css']
+})
+export class KanbanNewTaskComponent implements OnInit {
+
+  constructor(
+    private ts: TaskService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+
+  add(taskTitle, taskDueDate, taskCategory) {
+      let todoJSON: Todo = {
+        "id": 0,
+        "title": taskTitle,
+        "createdOn": null,
+        "completed": false,
+        "user": null,
+        "dueDate": taskDueDate,
+        "category": taskCategory,
+        "priority": false
+      };
+      this.ts.addTodo(todoJSON).subscribe();
+  }
+}
