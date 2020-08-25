b8c8c2d8d1cb66b725d1ffb44d22cbb3c853f817
Sandy Moose <sandra.moose@gmail.com>
2020-08-24 17:21:52 -0700

    router update for new task component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TaskListComponent } from './task-list/task-list.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+
 import { KanbanBoardComponent } from './kanban/kanban-board/kanban-board.component';
 import { KanbanDetailComponent } from './kanban/kanban-detail/kanban-detail.component';
 
@@ -11,7 +15,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
   { path: 'todos', component: TaskListComponent},
   { path: 'todos/:id', component: TaskDetailComponent},
-  { path: 'todos/add', component: TaskDetailComponent},
+  { path: 'todos/add', component: NewTaskComponent},
   { path: 'kanban', component: KanbanBoardComponent},
   { path: 'kanban/:id', component: KanbanDetailComponent},
   { path: 'kanban/add', component: KanbanDetailComponent},
